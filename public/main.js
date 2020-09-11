afd2fcc7f286862c7cefac219c4f71f1185a4ef3
Osaro Ero <osaro.ero@EPAYPLUSNG.LOCAL>
2020-09-11 22:27:47 +0100

    first commit

---
+++ public/main.js
@@ -0,0 +1,24 @@
+var button = document.querySelector("button");
+var inputValue = document.querySelector(".inputValue");
+var name = document.querySelector(".name");
+var desc = document.querySelector(".desc");
+var temp = document.querySelector(".temp");
+
+button.addEventListener("click", function () {
+  fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      inputValue.value +
+      "&appid=116be69233b84bb8a90d13c134c4e8f2"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      var nameValue = data["name"];
+      var tempValue = data["main"]["temp"];
+      var descValue = data["weather"][0]["description"];
+
+      name.innerHTML = nameValue;
+      temp.innerHTML = tempValue;
+      desc.innerHTML = descValue;
+    })
+    .catch((err) => alert("Wrong city name"));
+});
